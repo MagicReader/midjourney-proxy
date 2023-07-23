30ac513439c82515b58bf3114f396b295e88f88b
yanghao_a <123456aA>
2023-07-23 20:38:29 +0800

    c

--- src/main/java/com/github/novicezk/midjourney/wss/handle/BlendMessageHandler.java
+++ src/main/java/com/github/novicezk/midjourney/wss/handle/BlendMessageHandler.java
@@ -10,6 +10,7 @@ import com.github.novicezk.midjourney.support.DiscordHelper;
 import com.github.novicezk.midjourney.support.Task;
 import com.github.novicezk.midjourney.support.TaskCondition;
 import com.github.novicezk.midjourney.util.ContentParseData;
+import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.utils.data.DataObject;
 import org.springframework.stereotype.Component;
@@ -27,6 +28,7 @@ import java.util.regex.Pattern;
  * 进度(update): **<https://s.mj.run/JWu6jaL1D-8> <https://s.mj.run/QhfnQY-l68o> --v 5.1** - <@1012983546824114217> (0%) (relaxed)
  * 完成(create): **<https://s.mj.run/JWu6jaL1D-8> <https://s.mj.run/QhfnQY-l68o> --v 5.1** - <@1012983546824114217> (relaxed)
  */
+@Slf4j
 @Component
 public class BlendMessageHandler extends MessageHandler {
 	private static final String CONTENT_REGEX = "\\*\\*(.*?)\\*\\* - <@\\d+> \\((.*?)\\)";
